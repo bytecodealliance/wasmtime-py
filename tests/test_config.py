d9067e4a7f713c41c40e6fdbfaa1c2821a740091
Alex Crichton <alex@alexcrichton.com>
2020-03-26 16:34:02 -0700

    Start to fill out Config

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,4 +5,21 @@ from wasmtime import *
 
 class TestConfig(unittest.TestCase):
     def test_smoke(self):
-        Config()
+        config = Config()
+        config.debug_info(True)
+        config.wasm_threads(True)
+        config.wasm_reference_types(True)
+        config.wasm_simd(True)
+        config.wasm_bulk_memory(True)
+        config.wasm_multi_value(True)
+        config.cranelift_debug_verifier(True)
+        config.strategy("cranelift")
+        config.strategy("auto")
+        config.strategy("lightbeam")
+        with self.assertRaises(RuntimeError):
+            config.strategy("nonexistent-strategy")
+        config.cranelift_opt_level("none")
+        config.cranelift_opt_level("speed_and_size")
+        config.cranelift_opt_level("speed")
+        with self.assertRaises(RuntimeError):
+            config.cranelift_opt_level("nonexistent-level")
--- wasmtime/config.py
+++ wasmtime/config.py
@@ -8,6 +8,61 @@ class Config(object):
     def __init__(self):
         self.__ptr__ = dll.wasm_config_new()
 
+    def debug_info(self, enable):
+        if not isinstance(enable, bool):
+            raise TypeError('expected a bool')
+        dll.wasmtime_config_debug_info_set(self.__ptr__, enable)
+
+    def wasm_threads(self, enable):
+        if not isinstance(enable, bool):
+            raise TypeError('expected a bool')
+        dll.wasmtime_config_wasm_threads_set(self.__ptr__, enable)
+
+    def wasm_reference_types(self, enable):
+        if not isinstance(enable, bool):
+            raise TypeError('expected a bool')
+        dll.wasmtime_config_wasm_reference_types_set(self.__ptr__, enable)
+
+    def wasm_simd(self, enable):
+        if not isinstance(enable, bool):
+            raise TypeError('expected a bool')
+        dll.wasmtime_config_wasm_simd_set(self.__ptr__, enable)
+
+    def wasm_bulk_memory(self, enable):
+        if not isinstance(enable, bool):
+            raise TypeError('expected a bool')
+        dll.wasmtime_config_wasm_bulk_memory_set(self.__ptr__, enable)
+
+    def wasm_multi_value(self, enable):
+        if not isinstance(enable, bool):
+            raise TypeError('expected a bool')
+        dll.wasmtime_config_wasm_multi_value_set(self.__ptr__, enable)
+
+    def strategy(self, strategy):
+        if strategy == "auto":
+            dll.wasmtime_config_strategy_set(self.__ptr__, 0)
+        elif strategy == "cranelift":
+            dll.wasmtime_config_strategy_set(self.__ptr__, 1)
+        elif strategy == "lightbeam":
+            dll.wasmtime_config_strategy_set(self.__ptr__, 2)
+        else:
+            raise RuntimeError("unknown strategy: " + str(strategy))
+
+    def cranelift_debug_verifier(self, enable):
+        if not isinstance(enable, bool):
+            raise TypeError('expected a bool')
+        dll.wasmtime_config_cranelift_debug_verifier_set(self.__ptr__, enable)
+
+    def cranelift_opt_level(self, opt_level):
+        if opt_level == "none":
+            dll.wasmtime_config_cranelift_opt_level_set(self.__ptr__, 0)
+        elif opt_level == "speed":
+            dll.wasmtime_config_cranelift_opt_level_set(self.__ptr__, 1)
+        elif opt_level == "speed_and_size":
+            dll.wasmtime_config_cranelift_opt_level_set(self.__ptr__, 2)
+        else:
+            raise RuntimeError("unknown opt level: " + str(opt_level))
+
     def __del__(self):
         if hasattr(self, '__ptr__'):
             dll.wasm_config_delete(self.__ptr__)
