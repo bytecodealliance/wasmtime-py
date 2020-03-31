0e4a166a90c94b5fe71ebb9081e46855fb812d50
Alex Crichton <alex@alexcrichton.com>
2020-03-31 14:52:38 -0700

    Attempt to claim

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wasmtime",
+    name="wasmtime2",
     version="0.1.0",
     author="The Wasmtime Project Developers",
     author_email="hello@bytecodealliance.org",
