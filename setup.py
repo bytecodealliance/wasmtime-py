0a72a3cb50de8b466a1376d5e06bfe422d788c44
Alex Crichton <alex@alexcrichton.com>
2020-03-31 13:45:19 -0700

    Fill out some more setup.py metadata

--- setup.py
+++ setup.py
@@ -11,8 +11,29 @@ setuptools.setup(
     description="A WebAssembly runtime",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license="Apache-2.0 WITH LLVM-exception",
     url="https://github.com/bytecodealliance/wasmtime-py",
-    packages=setuptools.find_packages(),
+    packages=['wasmtime'],
     python_requires='>=2.7',
     test_suite="tests",
+    install_requires=['setuptools >= 36'],
+    extras_require={
+        'testing': [
+            'coverage',
+            'pytest',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Compilers',
+        'Topic :: Software Development :: Interpreters',
+        'Programming Language :: Rust',
+    ]
 )
