225b8148efa1d74b50923d3824c7e9688f3886a8
Alex Crichton <alex@alexcrichton.com>
2020-07-08 07:05:38 -0700

    Bump to 0.18.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "0.18.1"
+version = "0.18.2"
 
 # Give unique version numbers to all commits so our publication-on-each commit
 # works on main
