856fe06e325dd3c414793713d968093af60597e2
Alex Crichton <alex@alexcrichton.com>
2021-09-01 07:15:06 -0700

    Try to fix test on Windows

--- tests/test_module.py
+++ tests/test_module.py
@@ -162,3 +162,7 @@ class TestModule(unittest.TestCase):
             module = Module.deserialize_file(engine, path)
             assert(len(module.imports) == 0)
             assert(len(module.exports) == 0)
+
+            # Run the destructor for `Module` which has an mmap to the file
+            # which prevents deletion on Windows.
+            module = None
