0ec00a9e0755aab9d42f31fb6eedaebc39c44c41
Matt McCormick <matt.mccormick@kitware.com>
2023-02-27 14:21:23 -0500

    Demonstrate preopen of tmpdir in linking example

--- examples/linking.py
+++ examples/linking.py
@@ -2,6 +2,8 @@
 
 from wasmtime import Engine, Store, Module, Linker, WasiConfig
 
+import tempfile
+
 engine = Engine()
 
 # Load and compile our two modules
@@ -13,18 +15,20 @@ linking2 = Module.from_file(engine, "examples/linking2.wat")
 linker = Linker(engine)
 linker.define_wasi()
 
-# Create a `Store` to hold instances, and configure wasi state
-store = Store(engine)
-wasi = WasiConfig()
-wasi.inherit_stdout()
-store.set_wasi(wasi)
+with tempfile.TemporaryDirectory() as tmpdirname:
+    # Create a `Store` to hold instances, and configure wasi state
+    store = Store(engine)
+    wasi = WasiConfig()
+    wasi.inherit_stdout()
+    wasi.preopen_dir(tmpdirname, tmpdirname)
+    store.set_wasi(wasi)
 
-# Instantiate our first module which only uses WASI, then register that
-# instance with the linker since the next linking will use it.
-linking2 = linker.instantiate(store, linking2)
-linker.define_instance(store, "linking2", linking2)
+    # Instantiate our first module which only uses WASI, then register that
+    # instance with the linker since the next linking will use it.
+    linking2 = linker.instantiate(store, linking2)
+    linker.define_instance(store, "linking2", linking2)
 
-# And with that we can perform the final link and the execute the module.
-linking1 = linker.instantiate(store, linking1)
-run = linking1.exports(store)["run"]
-run(store)
+    # And with that we can perform the final link and the execute the module.
+    linking1 = linker.instantiate(store, linking1)
+    run = linking1.exports(store)["run"]
+    run(store)
