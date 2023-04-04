e6c054b02b345231b3d0108437c06d63bd823914
Muayyad alsadi <alsadi@gmail.com>
2023-04-05 02:18:51 +0300

    FIXES #137: make calling wasm from python 7x faster

--- examples/gcd_perf.py
+++ examples/gcd_perf.py
@@ -2,8 +2,6 @@ import wasmtime.loader
 import time
 from math import gcd as math_gcd
 from gcd import gcd_func as wasm_gcd
-from gcd_alt import gcd as wasm_gcd_alt
-
 
 def python_gcd(x, y):
     while y:
@@ -13,7 +11,7 @@ def python_gcd(x, y):
 
 N = 1_000
 by_name = locals()
-for name in "math_gcd", "python_gcd", "wasm_gcd", "wasm_gcd_alt":
+for name in "math_gcd", "python_gcd", "wasm_gcd":
     gcdf = by_name[name]
     start_time = time.perf_counter()
     for _ in range(N):
