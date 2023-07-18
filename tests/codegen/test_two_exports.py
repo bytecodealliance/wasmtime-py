bytecodealliance/wasmtime-py

commit ab9dffd8967e80748c1b0911ae727ed9b5142f54

Update to use new WIT import/export syntax (#159)
This commit is an attempt to update wasmtime-py to be able to handle
the new WIT import/export syntax.

Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

rust/bindgen/src/bindgen.rs
@@ -77,7 +77,7 @@ pub struct WasmtimePy {
 impl WasmtimePy {
     /// Generate bindings to load and instantiate the specific binary component
     /// provided.
-    pub fn generate(&mut self, name: &str, binary: &[u8], files: &mut Files) -> Result<()> {
+    pub fn generate(&mut self, _name: &str, binary: &[u8], files: &mut Files) -> Result<()> {
         // Use the `wit-component` crate here to parse `binary` and discover
         // the type-level descriptions and `Interface`s corresponding to the
         // component binary. This is effectively a step that infers a "world" of
@@ -85,7 +85,7 @@ impl WasmtimePy {
         // will likely change as worlds are iterated on in the component model
         // standard. Regardless though this is the step where types are learned
         // and `Interface`s are constructed for further code generation below.
-        let (resolve, id) = match wit_component::decode(name, binary)
+        let (resolve, id) = match wit_component::decode(binary)
             .context("failed to extract interface information from component")?
         {
             DecodedWasm::Component(resolve, world) => (resolve, world),
@@ -117,28 +117,42 @@ impl WasmtimePy {
         let (component, modules) = Translator::new(&tunables, &mut validator, &mut types, &scope)
             .translate(binary)
             .context("failed to parse the input component")?;
+        let world = &resolve.worlds[id];
 
         // Insert all core wasm modules into the generated `Files` which will
         // end up getting used in the `generate_instantiate` method.
         for (i, module) in modules.iter() {
-            files.push(&self.core_file_name(name, i.as_u32()), module.wasm);
+            files.push(&self.core_file_name(&world.name, i.as_u32()), module.wasm);
         }
 
         // With all that prep work delegate to `generate` here
         // to generate all the type-level descriptions for this component now
         // that the interfaces in/out are understood.
-        let world = &resolve.worlds[id];
-        for (name, import) in world.imports.iter() {
-            match import {
+        for (world_key, world_item) in world.imports.clone().into_iter() {
+            match world_item {
                 WorldItem::Function(_) => unimplemented!(),
-                WorldItem::Interface(id) => self.import_interface(&resolve, name, *id, files),
+                WorldItem::Interface(id) => {
+                    let interface = &resolve.interfaces[id];
+                    let iface_name = match world_key {
+                        WorldKey::Name(name) => name,
+                        WorldKey::Interface(_) => interface.name.as_ref().unwrap().to_string(),
+                    };
+                    self.import_interface(&resolve, &iface_name, id, files)
+                }
                 WorldItem::Type(_) => unimplemented!(),
             }
         }
-        for (name, export) in world.exports.iter() {
+        for (world_key, export) in world.exports.clone().into_iter() {
             match export {
                 WorldItem::Function(_) => {}
-                WorldItem::Interface(id) => self.export_interface(&resolve, name, *id, files),
+                WorldItem::Interface(id) => {
+                    let interface = &resolve.interfaces[id];
+                    let iface_name = match world_key {
+                        WorldKey::Name(name) => name,
+                        WorldKey::Interface(_) => interface.name.as_ref().unwrap().to_string(),
+                    };
+                    self.export_interface(&resolve, &iface_name, id, files)
+                }
                 WorldItem::Type(_) => unreachable!(),
             }
         }
@@ -149,7 +163,7 @@ impl WasmtimePy {
         // `wasmtime-environ` parsed.
         self.instantiate(&resolve, id, &component, &modules);
 
-        self.finish_component(name, files);
+        self.finish_component(&world.name, files);
 
         Ok(())
     }
@@ -256,7 +270,11 @@ impl WasmtimePy {
         i.gen.init.dedent();
 
         i.generate_lifts(&camel, None, &lifts);
-        for (name, lifts) in nested {
+        for (package_name, lifts) in nested {
+            let name = match package_name.find("/") {
+                Some(pos) => package_name.split_at(pos + 1).1,
+                None => &package_name,
+            };
             i.generate_lifts(&camel, Some(name), &lifts);
         }
         i.gen.init.dedent();
@@ -508,7 +526,8 @@ impl<'a> Instantiator<'a> {
         // where instances export functions.
         let (import_index, path) = &self.component.imports[import.import];
         let (import_name, _import_ty) = &self.component.import_types[*import_index];
-        let item = &self.resolve.worlds[self.world].imports[import_name.as_str()];
+        let index: usize = import_index.as_u32() as usize;
+        let item = &self.resolve.worlds[self.world].imports[index];
         let (func, interface) = match item {
             WorldItem::Function(f) => {
                 assert_eq!(path.len(), 0);
@@ -522,6 +541,11 @@ impl<'a> Instantiator<'a> {
         };
 
         let index = import.index.as_u32();
+        let import_name = import_name.replace(":", ".");
+        let import_name = match import_name.find("/") {
+            Some(pos) => import_name.split_at(pos + 1).1,
+            None => &import_name,
+        };
         let callee = format!(
             "import_object.{}.{}",
             import_name.to_snake_case(),
@@ -700,7 +724,6 @@ impl<'a> Instantiator<'a> {
         let mut nested = BTreeMap::new();
         for (name, export) in exports {
             let name = name.as_str();
-            let item = &self.resolve.worlds[self.world].exports[name];
             match export {
                 Export::LiftedFunction {
                     ty: _,
@@ -708,19 +731,28 @@ impl<'a> Instantiator<'a> {
                     options,
                 } => {
                     let callee = self.gen_lift_callee(func);
-                    let func = match item {
-                        WorldItem::Function(f) => f,
-                        WorldItem::Interface(_) | WorldItem::Type(_) => unreachable!(),
-                    };
-                    toplevel.push(Lift {
-                        callee,
-                        opts: options,
-                        func,
-                        interface: None,
-                    });
+                    if let Some((_, item)) = &self.resolve.worlds[self.world].exports.iter().find(
+                        |&(key, _)| match key {
+                            WorldKey::Name(function_name) => function_name == name,
+                            _ => false,
+                        },
+                    ) {
+                        let func = match item {
+                            WorldItem::Function(f) => f,
+                            WorldItem::Interface(_) | WorldItem::Type(_) => unreachable!(),
+                        };
+                        toplevel.push(Lift {
+                            callee,
+                            opts: options,
+                            func,
+                            interface: None,
+                        });
+                    }
                 }
 
                 Export::Instance(exports) => {
+                    let wid = self.world.index();
+                    let item = &self.resolve.worlds[self.world].exports[wid];
                     let id = match item {
                         WorldItem::Interface(id) => *id,
                         WorldItem::Function(_) | WorldItem::Type(_) => unreachable!(),
@@ -900,7 +932,10 @@ impl InterfaceGenerator<'_> {
                     } else {
                         let (module, iface) = match self.gen.imported_interfaces.get(&owner) {
                             Some(name) => ("imports", name),
-                            None => ("exports", &self.gen.exported_interfaces[&owner]),
+                            None => (
+                                "exports",
+                                self.gen.exported_interfaces.get(&owner).unwrap_or(name),
+                            ),
                         };
                         let module = if self.at_root {
                             format!(".{module}")
@@ -1302,7 +1337,7 @@ impl InterfaceGenerator<'_> {
         let id = self.interface.unwrap();
         let (module, iface) = match self.gen.imported_interfaces.get(&id) {
             Some(name) => (".imports", name),
-            None => (".exports", &self.gen.exported_interfaces[&id]),
+            None => (".exports", self.gen.exported_interfaces.get(&id).unwrap()),
         };
         let iface = iface.to_snake_case();
         self.src.pyimport(&module, iface.as_str());

wasmtime/bindgen/__init__.py
@@ -1,28 +1,106 @@
-from .generated import Bindgen, BindgenImports, Err
-from typing import Mapping, Tuple
+from .generated import Root, RootImports, Err
+from .generated.imports import Exit
+from .generated.imports import Random
+from .generated.imports import Stdin
+from .generated.imports import Stdout
+from .generated.imports import Stderr
+from .generated.imports import streams
+from .generated.imports import Preopens
+from .generated.imports import Environment
+from .generated.imports import Filesystem
+from .generated.imports.filesystem import Descriptor, Filesize, ErrorCode, DescriptorType
+from .generated import types as core_types
+from typing import Mapping, Tuple, List
+
 import sys
+import os
 from wasmtime import Store
 
 
-class Imports:
-    def print(self, list: bytes) -> None:
-        sys.stdout.buffer.write(list)
-
-    def eprint(self, list: bytes) -> None:
-        sys.stderr.buffer.write(list)
+class WasiRandom(Random):
+    def get_random_bytes(self, len: int) -> bytes:
+        return os.urandom(len)
 
 
-bindgen = None
+class WasiStdin(Stdin):
+    def get_stdin(self) -> streams.InputStream:
+        return sys.stdin.fileno()
+
+
+class WasiStdout(Stdout):
+    def get_stdout(self) -> streams.OutputStream:
+        return sys.stdout.fileno()
+
+
+class WasiStderr(Stderr):
+    def get_stderr(self) -> streams.OutputStream:
+        return sys.stderr.fileno()
+
+
+class WasiPreopens(Preopens):
+    def get_directories(self) -> List[Tuple[Descriptor, str]]:
+        return []
+
+
+class WasiStreams(streams.Streams):
+    def drop_input_stream(self, this: streams.InputStream) -> None:
+        return None
+
+    def write(self, this: streams.OutputStream, buf: bytes) -> core_types.Result[int, streams.StreamError]:
+        sys.stdout.buffer.write(buf)
+        return core_types.Ok(len(buf))
+
+    def blocking_write(self, this: streams.OutputStream, buf: bytes) -> core_types.Result[int, streams.StreamError]:
+        sys.stdout.buffer.write(buf)
+        return core_types.Ok(len(buf))
+
+    def drop_output_stream(self, this: streams.OutputStream) -> None:
+        return None
+
+
+class WasiEnvironment(Environment):
+    def get_environment(self) -> List[Tuple[str, str]]:
+        return []
+
+
+class WasiFilesystem(Filesystem):
+    def write_via_stream(self, this: Descriptor, offset: Filesize) -> core_types.Result[streams.OutputStream, ErrorCode]:
+        raise NotImplementedError
+
+    def append_via_stream(self, this: Descriptor) -> core_types.Result[streams.OutputStream, ErrorCode]:
+        raise NotImplementedError
+
+    def get_type(self, this: Descriptor) -> core_types.Result[DescriptorType, ErrorCode]:
+        raise NotImplementedError
+
+    def drop_descriptor(self, this: Descriptor) -> None:
+        raise NotImplementedError
+
+
+class WasiExit(Exit):
+    def exit(self, status: core_types.Result[None, None]) -> None:
+        raise NotImplementedError
+
+
+root = None
 store = None
 
 
-def init() -> Tuple[Bindgen, Store]:
+def init() -> Tuple[Root, Store]:
     global store
-    global bindgen
-    if bindgen is None:
+    global root
+    if root is None:
         store = Store()
-        bindgen = Bindgen(store, BindgenImports(python=Imports()))
-    return bindgen, store
+        root = Root(store, RootImports(WasiStreams(),
+                                       WasiFilesystem(),
+                                       WasiRandom(),
+                                       WasiEnvironment(),
+                                       WasiPreopens(),
+                                       WasiExit(),
+                                       WasiStdin(),
+                                       WasiStdout(),
+                                       WasiStderr()))
+    return root, store
 
 
 # Generates Python bindings for the given component.
@@ -34,8 +112,8 @@ def init() -> Tuple[Bindgen, Store]:
 # This function returns a mapping of filename to contents of files that are
 # generated to represent the Python bindings here.
 def generate(name: str, component: bytes) -> Mapping[str, bytes]:
-    bindgen, store = init()
-    result = bindgen.generate(store, name, component)
+    root, store = init()
+    result = root.generate(store, name, component)
     if isinstance(result, Err):
         raise RuntimeError(result.value)
     ret = {}

.github/actions/setup/action.yml
@@ -34,7 +34,7 @@ runs:
   # Install the `wasm-tools` binary with the `component` subcommand that is all
   # that's needed here.
   - run: |
-      echo WASM_TOOLS=wasm-tools-1.0.23 >> $GITHUB_ENV
+      echo WASM_TOOLS=wasm-tools-1.0.35 >> $GITHUB_ENV
       mkdir -p '${{ runner.tool_cache }}/wasm-tools'
       echo '${{ runner.tool_cache }}/wasm-tools' >> $GITHUB_PATH
     shell: bash

ci/build-rust.py
@@ -13,7 +13,6 @@
 # remains the same.
 
 import subprocess
-import os
 
 
 def main():
@@ -24,24 +23,14 @@ def main():
         cwd='rust'
     ).check_returncode()
 
-    print('======================= Building wasi_snapshot_preview1.wasm ======')
-
-    env = os.environ.copy()
-    env['RUSTFLAGS'] = '-Clink-arg=--import-memory -Clink-arg=-zstack-size=0'
-    subprocess.run(
-        ['cargo', 'build', '--release', '--target', 'wasm32-unknown-unknown', '-p', 'wasi_snapshot_preview1'],
-        cwd='rust',
-        env=env,
-    ).check_returncode()
-
     core = 'rust/target/wasm32-wasi/release/bindgen.wasm'
-    wasi = 'rust/target/wasm32-unknown-unknown/release/wasi_snapshot_preview1.wasm'
+    wasi = 'ci/wasi_preview1_component_adapter.wasm'
     component = 'rust/target/component.wasm'
 
     print('======================= Building component.wasm ===================')
 
     subprocess.run(
-        ['wasm-tools', 'component', 'new', core, '--adapt', wasi, '-o', component],
+        ['wasm-tools', 'component', 'new', core, '--adapt', f'wasi_snapshot_preview1={wasi}', '-o', component],
     ).check_returncode()
 
     print('======================= Bootstrapping with native platform ========')

ci/download-wasmtime.py
@@ -10,7 +10,7 @@ import tarfile
 import urllib.request
 import zipfile
 
-WASMTIME_VERSION = "v9.0.0"
+WASMTIME_VERSION = "v10.0.0"
 
 
 def main(platform, arch):

rust/Cargo.toml
@@ -2,7 +2,7 @@
 members = ['bindgen', 'wasi_snapshot_preview1']
 
 [workspace.dependencies]
-wit-bindgen = { version = "0.5.0", default-features = false, features = ['macros'] }
+wit-bindgen = { version = "0.8.0", default-features = false, features = ['macros'] }
 
 [profile.release]
 strip = 'debuginfo'

rust/bindgen.wit
@@ -1,3 +1,5 @@
-default world wasmtime-py {
+package wasmtime:python
+
+world bindgen {
   export generate: func(name: string, wit: list<u8>) -> result<list<tuple<string, list<u8>>>, string>
 }

commit d93e06fe3410a0250250fe8fb108cfabd967e2c3

Fix lookup of exports in worlds
This fixes an accidental regression from #159 where exports were looked
up incorrectly in the target world by index rather than name. This fixes
the bug by building a temporary map from string to item instead of
`WorldKey` to item which is consulted.

Closes #161

rust/bindgen/src/bindgen.rs
@@ -722,6 +722,11 @@ impl<'a> Instantiator<'a> {
     ) -> (Vec<Lift<'a>>, BTreeMap<&'a str, Vec<Lift<'a>>>) {
         let mut toplevel = Vec::new();
         let mut nested = BTreeMap::new();
+        let world_exports_by_string = self.resolve.worlds[self.world]
+            .exports
+            .iter()
+            .map(|(k, v)| (self.resolve.name_world_key(k), v))
+            .collect::<HashMap<_, _>>();
         for (name, export) in exports {
             let name = name.as_str();
             match export {
@@ -731,29 +736,20 @@ impl<'a> Instantiator<'a> {
                     options,
                 } => {
                     let callee = self.gen_lift_callee(func);
-                    if let Some((_, item)) = &self.resolve.worlds[self.world].exports.iter().find(
-                        |&(key, _)| match key {
-                            WorldKey::Name(function_name) => function_name == name,
-                            _ => false,
-                        },
-                    ) {
-                        let func = match item {
-                            WorldItem::Function(f) => f,
-                            WorldItem::Interface(_) | WorldItem::Type(_) => unreachable!(),
-                        };
-                        toplevel.push(Lift {
-                            callee,
-                            opts: options,
-                            func,
-                            interface: None,
-                        });
-                    }
+                    let func = match world_exports_by_string[name] {
+                        WorldItem::Function(f) => f,
+                        WorldItem::Interface(_) | WorldItem::Type(_) => unreachable!(),
+                    };
+                    toplevel.push(Lift {
+                        callee,
+                        opts: options,
+                        func,
+                        interface: None,
+                    });
                 }
 
                 Export::Instance(exports) => {
-                    let wid = self.world.index();
-                    let item = &self.resolve.worlds[self.world].exports[wid];
-                    let id = match item {
+                    let id = match world_exports_by_string[name] {
                         WorldItem::Interface(id) => *id,
                         WorldItem::Function(_) | WorldItem::Type(_) => unreachable!(),
                     };

wasmtime/bindgen/__init__.py
@@ -24,17 +24,17 @@ class WasiRandom(Random):
 
 class WasiStdin(Stdin):
     def get_stdin(self) -> streams.InputStream:
-        return sys.stdin.fileno()
+        return 0
 
 
 class WasiStdout(Stdout):
     def get_stdout(self) -> streams.OutputStream:
-        return sys.stdout.fileno()
+        return 1
 
 
 class WasiStderr(Stderr):
     def get_stderr(self) -> streams.OutputStream:
-        return sys.stderr.fileno()
+        return 2
 
 
 class WasiPreopens(Preopens):
@@ -47,12 +47,16 @@ class WasiStreams(streams.Streams):
         return None
 
     def write(self, this: streams.OutputStream, buf: bytes) -> core_types.Result[int, streams.StreamError]:
-        sys.stdout.buffer.write(buf)
+        if this == 1:
+            sys.stdout.buffer.write(buf)
+        elif this == 2:
+            sys.stderr.buffer.write(buf)
+        else:
+            raise NotImplementedError
         return core_types.Ok(len(buf))
 
     def blocking_write(self, this: streams.OutputStream, buf: bytes) -> core_types.Result[int, streams.StreamError]:
-        sys.stdout.buffer.write(buf)
-        return core_types.Ok(len(buf))
+        return self.write(this, buf)
 
     def drop_output_stream(self, this: streams.OutputStream) -> None:
         return None

tests/codegen/foo.wat
@@ -1,0 +1,17 @@
+
+    (component
+        (import "i" (instance $i
+          (export "f1" (func))
+          (export "f2" (func))
+        ))
+
+        (core func $f1 (canon lower (func $i "f1")))
+        (core func $f2 (canon lower (func $i "f2")))
+
+        (func $f1' (canon lift (core func $f1)))
+        (func $f2' (canon lift (core func $f2)))
+
+        (instance (export "i1")
+            (export "f1" (func $f1')))
+        ;;(export "i2" (instance (export "f2" (func $f2))))
+    )

tests/codegen/test_two_exports.py
@@ -1,0 +1,42 @@
+from . import bindgen
+from wasmtime import Store
+
+module = """
+    (component
+        (import "i" (instance $i
+          (export "f1" (func))
+          (export "f2" (func))
+        ))
+
+        (core func $f1 (canon lower (func $i "f1")))
+        (core func $f2 (canon lower (func $i "f2")))
+
+        (func $f1' (canon lift (core func $f1)))
+        (func $f2' (canon lift (core func $f2)))
+
+        (instance (export "i1") (export "f1" (func $f1')))
+        (instance (export "i2") (export "f2" (func $f2')))
+    )
+"""
+bindgen('two_exports', module)
+
+from .generated.two_exports import Root, RootImports, imports
+
+
+class Host(imports.I):
+    def f1(self) -> None:
+        self.f1_hit = True
+
+    def f2(self) -> None:
+        self.f2_hit = True
+
+
+def test_bindings():
+    store = Store()
+    host = Host()
+    wasm = Root(store, RootImports(i=host))
+
+    wasm.i1().f1(store)
+    assert(host.f1_hit)
+    wasm.i2().f2(store)
+    assert(host.f2_hit)