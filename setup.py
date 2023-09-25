f319f4c684bbff3f6db2cd281444ef87cc3d4334
Alex Crichton <alex@alexcrichton.com>
2023-09-25 09:11:37 -0700

    Bump to 13.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import subprocess
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "13.0.1"
+version = "13.0.2"
 
 # Give unique version numbers to all commits so our publication-on-each commit
 # works on main
