6d82a9511858a6f92bb25969f801a31015d350b0
Juan Gomez Mosquera <atilag@gmail.com>
2024-11-24 20:39:02 -0500

    * Fixing Python types as suggested by mypy * Adding some unit tests for shared memory

--- wasmtime/_exportable.py
+++ wasmtime/_exportable.py
@@ -4,8 +4,9 @@ if typing.TYPE_CHECKING:
     from ._func import Func
     from ._globals import Global
     from ._memory import Memory
+    from ._sharedmemory import SharedMemory
     from ._table import Table
     from ._module import Module
     from ._instance import Instance
 
-AsExtern = typing.Union["Func", "Table", "Memory", "Global", "Instance", "Module"]
+AsExtern = typing.Union["Func", "Table", "Memory", "SharedMemory",  "Global", "Instance", "Module"]
--- wasmtime/_sharedmemory.py
+++ wasmtime/_sharedmemory.py
@@ -1,6 +1,7 @@
 from . import _ffi as ffi
 from ctypes import *
 import ctypes
+from typing import Optional, Any
 from wasmtime import MemoryType, WasmtimeError, Engine, Managed
 from ._store import Storelike
 
@@ -12,20 +13,22 @@ class SharedMemory(Managed["ctypes._Pointer[ffi.wasmtime_sharedmemory_t]"]):
         Creates a new shared memory in `store` with the given `ty`
         """
 
-        sharedmemory = ffi.wasmtime_sharedmemory_t()
-        ptr = POINTER(ffi.wasmtime_sharedmemory_t)()
-        error = ffi.wasmtime_sharedmemory_new(engine.ptr(), ty.ptr(), byref(ptr))
+        sharedmemory_ptr = POINTER(ffi.wasmtime_sharedmemory_t)()
+        error = ffi.wasmtime_sharedmemory_new(engine.ptr(), ty.ptr(), byref(sharedmemory_ptr))
         if error:
             raise WasmtimeError._from_ptr(error)
-        self._set_ptr(sharedmemory)
+        self._set_ptr(sharedmemory_ptr)
+
+    def _delete(self, ptr: "ctypes._Pointer[ffi.wasmtime_sharedmemory_t]") -> None:
+        ffi.wasmtime_sharedmemory_delete(ptr)
 
     @classmethod
-    def _from_raw(cls, sharedmemory: ffi.wasmtime_sharedmemory_t) -> "SharedMemory":
-        if not isinstance(sharedmemory, ffi.wasmtime_sharedmemory_t):
-            raise TypeError("wrong shared memory pointer type provided to _from_raw")
+    def _from_ptr(cls, ptr: "ctypes._Pointer[ffi.wasmtime_sharedmemory_t]") -> "SharedMemory":
+        if not isinstance(ptr, POINTER(ffi.wasmtime_sharedmemory_t)):
+            raise TypeError("wrong shared memory pointer type provided to _from_ptr")
         
         ty: "SharedMemory" = cls.__new__(cls)
-        ty._set_ptr(sharedmemory)
+        ty._set_ptr(ptr)
         return ty
 
     def type(self) -> MemoryType:
@@ -33,7 +36,7 @@ class SharedMemory(Managed["ctypes._Pointer[ffi.wasmtime_sharedmemory_t]"]):
         Gets the type of this memory as a `MemoryType`
         """
 
-        ptr = ffi.wasmtime_sharedmemory_type(byref(self._sharedmemory))
+        ptr = ffi.wasmtime_sharedmemory_type(self.ptr())
         return MemoryType._from_ptr(ptr, None)
 
     def grow(self, delta: int) -> int:
@@ -44,7 +47,7 @@ class SharedMemory(Managed["ctypes._Pointer[ffi.wasmtime_sharedmemory_t]"]):
         if delta < 0:
             raise WasmtimeError("cannot grow by negative amount")
         prev = ffi.c_uint64(0)
-        error = ffi.wasmtime_sharedmemory_grow(byref(self.ptr()), delta, byref(prev))
+        error = ffi.wasmtime_sharedmemory_grow(self.ptr(), delta, byref(prev))
         if error:
             raise WasmtimeError._from_ptr(error)
         return prev.value
@@ -63,14 +66,14 @@ class SharedMemory(Managed["ctypes._Pointer[ffi.wasmtime_sharedmemory_t]"]):
         Remember that all accesses to wasm shared memory should be bounds-checked
         against the `data_len` method.
         """
-        return ffi.wasmtime_sharedmemory_data(byref(self.ptr()))
+        return ffi.wasmtime_sharedmemory_data(self.ptr())
 
     def data_len(self) -> int:
         """
         Returns the raw byte length of this memory.
         """
 
-        return ffi.wasmtime_sharedmemory_data_size(byref(self.ptr()))
+        return ffi.wasmtime_sharedmemory_data_size(self.ptr())
 
     def _as_extern(self) -> ffi.wasmtime_extern_t:
         union = ffi.wasmtime_extern_union(sharedmemory=pointer(self.ptr()))
