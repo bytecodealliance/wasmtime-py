3bc30c6bc600bd95af812e96ccbdd43a38d4ce96
Alex Crichton <alex@alexcrichton.com>
2025-07-22 13:16:53 -0700

    Bump to 35.0.0

--- ci/download-wasmtime.py
+++ ci/download-wasmtime.py
@@ -12,7 +12,7 @@ from pathlib import Path
 
 # set to "dev" to download the latest or pick a tag from
 # https://github.com/bytecodealliance/wasmtime/tags
-WASMTIME_VERSION = "v34.0.0"
+WASMTIME_VERSION = "v35.0.0"
 
 
 def main(platform, arch):
