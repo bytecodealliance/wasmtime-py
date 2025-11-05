fb42211937fac78cb7fc8d80ee7d6aa6697bd8c7
Alex Crichton <alex@alexcrichton.com>
2025-11-05 14:05:35 -0800

    Implement capturing stdout/stderr in Python
    Bind a recent addition to the C API
    
    Closes #34

--- tests/test_wasi.py
+++ tests/test_wasi.py
@@ -52,3 +52,65 @@ class TestWasi(unittest.TestCase):
         config = WasiConfig()
         with self.assertRaises(WasmtimeError):
             config.preopen_dir('/path/to/nowhere', '/', DirPerms.READ_ONLY, FilePerms.READ_ONLY)
+
+    def test_custom_print(self):
+        linker = Linker(Engine())
+        linker.define_wasi()
+
+        stderr = ''
+        stdout = ''
+
+        def on_stdout(data: bytes) -> None:
+            nonlocal stdout
+            stdout += data.decode('utf8')
+
+        def on_stderr(data: bytes) -> None:
+            nonlocal stderr
+            stderr += data.decode('utf8')
+
+        module = Module(linker.engine, """
+            (module
+                (import "wasi_snapshot_preview1" "fd_write"
+                    (func $write (param i32 i32 i32 i32) (result i32)))
+
+                (memory (export "memory") 1)
+
+                (func $print
+                    (i32.store (i32.const 300) (i32.const 100)) ;; iov base
+                    (i32.store (i32.const 304) (i32.const 14))  ;; iov len
+
+                    (call $write
+                        (i32.const 1)    ;; fd 1 is stdout
+                        (i32.const 300)  ;; iovecs ptr
+                        (i32.const 1)    ;; iovecs len
+                        (i32.const 400)) ;; nwritten ptr
+                    if unreachable end   ;; verify no error
+
+                    (i32.store (i32.const 300) (i32.const 200)) ;; iov base
+                    (i32.store (i32.const 304) (i32.const 14))  ;; iov len
+
+                    (call $write
+                        (i32.const 2)    ;; fd 2 is stderr
+                        (i32.const 300)  ;; iovecs ptr
+                        (i32.const 1)    ;; iovecs len
+                        (i32.const 400)) ;; nwritten ptr
+                    if unreachable end   ;; verify no error
+                )
+
+                (start $print)
+
+                (data (i32.const 100) "Hello, stdout!")
+                (data (i32.const 200) "Hello, stderr!")
+            )
+        """)
+
+        wasi = WasiConfig()
+        wasi.stdout_custom = on_stdout
+        wasi.stderr_custom = on_stderr
+
+        store = Store(linker.engine)
+        store.set_wasi(wasi)
+        linker.instantiate(store, module)
+
+        self.assertEqual('Hello, stdout!', stdout)
+        self.assertEqual('Hello, stderr!', stderr)
