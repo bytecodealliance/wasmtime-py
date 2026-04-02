b030baab59f4fc0429ddc6a03c24c085aacc49b8
Chay Nabors <nabochay@amazon.com>
2026-04-02 15:31:40 -0400

    Add wasi:http support

--- wasmtime/component/_linker.py
+++ wasmtime/component/_linker.py
@@ -73,6 +73,18 @@ class Linker(Managed["ctypes._Pointer[ffi.wasmtime_component_linker_t]"]):
         if err:
             raise WasmtimeError._from_ptr(err)
 
+    def add_wasi_http(self) -> None:
+        """
+        Adds the WASI HTTP API definitions to this linker.
+
+        This adds `wasi:http/types` and `wasi:http/outgoing-handler`.
+        Requires WASIp2 to be added first via `add_wasip2()`.
+        """
+        self._assert_not_locked()
+        err = ffi.wasmtime_component_linker_add_wasi_http(self.ptr())
+        if err:
+            raise WasmtimeError._from_ptr(err)
+
     def instantiate(self, store: Storelike, component: Component) -> Instance:
         """
         Instantiates the given component using this linker within the provided
